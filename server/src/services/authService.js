1a704e084e47bc6d24fef6e638e1d4bbf862c634
Shreshma <155938846+ShreshmaSiriMary@users.noreply.github.com>
2026-07-19 14:19:06 +0530

    Implement signup and login methods in AuthService

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,5 +1,46 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
 class AuthService {
-  // Auth business logic
+  async signup({ name, email, password }) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error('User already exists');
+      error.statusCode = 409;
+      throw error;
+    }
+
+    const user = await User.create({ name, email, password });
+    const token = generateToken(user._id);
+
+    return {
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    };
+  }
+
+  async login({ email, password }) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error('Invalid credentials');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      const error = new Error('Invalid credentials');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = generateToken(user._id);
+
+    return {
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    };
+  }
 }
 
 module.exports = new AuthService();
