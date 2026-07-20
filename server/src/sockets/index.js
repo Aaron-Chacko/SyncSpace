4a84de75fecdd2cf464a783a3d57696b6fd319b6
Mayank Patel <mayankpatelmehta@gmail.com>
2026-07-20 14:22:04 +0530

    Migrate whiteboard backend modules to ESM (.mjs)

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -1,8 +1,7 @@
-const whiteboardSocketHandler = require('./whiteboardSocket');
 const editorSocketHandler = require('./editorSocket');
 
 module.exports = (io) => {
-  io.on('connection', (socket) => {
+  io.on('connection', async (socket) => {
     console.log('Socket connected: ' + socket.id);
 
     socket.on('join-room', (roomId) => {
@@ -10,7 +9,13 @@ module.exports = (io) => {
       console.log('Socket ' + socket.id + ' joined room ' + roomId);
     });
 
-    whiteboardSocketHandler(io, socket);
+    try {
+      const { default: whiteboardSocketHandler } = await import('./whiteboardSocket.mjs');
+      whiteboardSocketHandler(io, socket);
+    } catch (err) {
+      console.error('Failed to load whiteboardSocket ESM module:', err);
+    }
+
     editorSocketHandler(io, socket);
 
     socket.on('disconnect', () => {
